c3a301e99017553517caf43e6ca11984cea75860
weston273 <sululuweston@gmail.com>
2025-07-01 14:39:49 +0200

    fixed splashscreen

--- SmartScribe_React/smartScribe/src/SplashScreen.jsx
+++ SmartScribe_React/smartScribe/src/SplashScreen.jsx
@@ -6,7 +6,7 @@ const SplashScreen = () => {
   return (
     <div className="splash-container">
       <img src={logo} alt="Smart Logo" className="splash-logo" /> 
-      <p className="loading-text">loading...</p>
+      {/* <p className="loading-text">loading...</p> */}
     </div>
   );
 };
