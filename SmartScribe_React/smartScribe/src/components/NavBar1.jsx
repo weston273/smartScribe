f1354993d2489659e13a54ce9a8cb9a48cc13511
weston273 <sululuweston@gmail.com>
2025-07-04 11:40:18 +0200

    updated

--- SmartScribe_React/smartScribe/src/components/NavBar1.jsx
+++ SmartScribe_React/smartScribe/src/components/NavBar1.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import "./Navbar1.css";
+import "./NavBar1.css";
 import Logo from './../assets/logo-nav.png';
 import Sidebar from './../assets/sidebar.png';
 import Profile from '../assets/profile.png';
