191e97916a2c7cfabb61d7a7c7f2aa3999d0d98a
weston273 <sululuweston@gmail.com>
2025-07-10 10:03:36 +0200

    + toggle now works

--- SmartScribe_React/smartScribe/src/components/Hero.jsx
+++ SmartScribe_React/smartScribe/src/components/Hero.jsx
@@ -1,23 +1,22 @@
 import React, {useState} from 'react'
 import './Hero.css'
 
-export default function Hero() {
+export default function Hero({ showInput }) {
     const [text, setText] = useState('');
 
   return (
-    
       <div className='hero-container' >
         <div className={text ? 'output-text' : 'placeholder-text'}>
         {text || 'SmartScribe'}
         </div>
-      <input
-        type="text"
-        value={text}
-        onChange={(e) => setText(e.target.value)}
-        placeholder="Type here..."
-      />
-     
+        {showInput && (
+            <input
+              type="text"
+              value={text}
+              onChange={(e) => setText(e.target.value)}
+              placeholder="Type here..."
+            />
+        )}
         </div>
-    
-  )
+  );
 }
--- SmartScribe_React/smartScribe/src/pages/Home.jsx
+++ SmartScribe_React/smartScribe/src/pages/Home.jsx
@@ -1,18 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import NavBar1 from '../components/NavBar1.jsx'
 import Hero from '../components/Hero.jsx'
 import Essentials from '../components/Essentials.jsx';
 import Footer from '../components/Footer.jsx';
+
+
 const Home = ({ theme, toggleTheme }) => {
+  // Visibility state for the input box
+  const [showInput, setShowInput] = useState(false);
+
+  // Function to toggle input visibility
+  const toggleInput = () => setShowInput(prev => !prev);
+
+  
+
   return (
     <>
       < NavBar1 theme ={theme} />
-      <Hero />
-      <Essentials />
+      <Hero showInput={showInput} />
+      <Essentials onNotesClick={toggleInput} />
       {/* Pass theme and toggleTheme to Footer */}
       <Footer theme={theme} toggleTheme={toggleTheme} />
     </>
-    
   );
 };
 
